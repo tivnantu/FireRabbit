4139938373432fdfdf176f1c59ee5043c30c6d90
tivnan <supdev@163.com>
2021-06-09 20:15:08 +0800

    实现添加书签及跳转书签界面功能

---
+++ app/src/main/java/cn/tivnan/firerabbit/dao/BookmarkDataHelper.java
@@ -0,0 +1,37 @@
+package cn.tivnan.firerabbit.dao;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class BookmarkDataHelper extends SQLiteOpenHelper {
+    //书签表
+    public static final String CREATE_bookmarkDB = "create table bookmark(" +
+            "id integer primary key autoincrement," +
+            "title text ," +
+            "url text)";
+
+    private Context mContext;
+
+    public BookmarkDataHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_bookmarkDB);
+        Log.d("BookmarkDataHelper", "onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists bookmark");
+        onCreate(db);
+    }
+}
