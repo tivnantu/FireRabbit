a22080671fea4bd4eba6d978fe0a95c5966d4707
tivnan <supdev@163.com>
2021-06-17 10:52:51 +0800

    history修复不能存储网页标题的问题

--- app/src/main/java/cn/tivnan/firerabbit/MainActivity.java
+++ app/src/main/java/cn/tivnan/firerabbit/MainActivity.java
@@ -161,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
                 super.onPageFinished(view, url);
 
                 if (if_load) {
-                    new HistoryController(MainActivity.this).addHistory(view.copyBackForwardList().getCurrentItem().getTitle(), view.copyBackForwardList().getCurrentItem().getUrl());
+                    new HistoryController(MainActivity.this).addHistory(view.getTitle(), view.getUrl());
                     if_load = false;
                 }
             }
